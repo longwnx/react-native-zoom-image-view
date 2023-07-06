1930ef2c6c6fde6ef092d52f051948d4b9b3fccd
Long Nguyen Xuan <nxl232@gmail.com>
2023-07-06 17:58:21 +0700

    fix: some bug

--- src/ImageView.tsx
+++ src/ImageView.tsx
@@ -43,7 +43,7 @@ interface Props {
   FooterComponent?: ComponentType<{ imageIndex: number }>;
   top?: number;
   cachePriority?: Priority;
-  resizeMode: ResizeMode;
+  resizeMode?: ResizeMode;
 }
 
 const DEFAULT_ANIMATION_TYPE = 'fade';
